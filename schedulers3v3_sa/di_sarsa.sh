f9612d6f291d3508d836cdf8b1e91b9820fff113
saikalyan9981 <saikalyan@cse.iitb.ac.in>
2020-03-09 15:24:07 +0530

    Clean up ignored files

--- schedulers3v3_sa/di_sarsa.sh
+++ schedulers3v3_sa/di_sarsa.sh
@@ -6,6 +6,8 @@ Step=32
 Port=7160
 Seed=1
 TrainEpisodes=50000
+TestEpisodes=2000
+
 
 stdbuf -oL ./HFO/bin/HFO --offense-npcs 3 --defense-npcs 2 --defense-agents 1 \
 --port ${Port} --no-logging --headless --deterministic --trials 52000 --seed $Seed > logs/di_sarsa.log 2>&1 &
@@ -14,7 +16,7 @@ PID=$!
 cd di_sarsa
 sleep 5
 
-./di_sarsa --numAgents 1 --numOpponents 3 --numEpisodes ${} --numEpisodesTest 2000 --basePort ${Port} \
+./di_sarsa --numAgents 1 --numOpponents 3 --numEpisodes ${TrainEpisodes} --numEpisodesTest ${TestEpisodes} --basePort ${Port} \
 --weightId di_sarsa_lambda_${Lambda}_step_${Step}_seed_${Seed} --lambda ${Lambda} --learnRate ${LearnR} --step ${Step} > ../logs/di_sarsa_debug.log
 
 kill -SIGINT $PID
